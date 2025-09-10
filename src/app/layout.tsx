94b172df5a9a98646f4e8799a85584268bce70be
Arthur Robson <arthurobson.contato@gmail.com>
2025-09-10 09:38:51 -0300

    style/layout: change dark mode color to gray 500

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-gray-800`}
       >
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
